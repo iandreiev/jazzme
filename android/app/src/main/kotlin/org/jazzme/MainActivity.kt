320ed3bbd2e602a1a54238520eaad058d54feaa3
Ivan Andreyev <andrv.dev@gmail.com>
2020-09-24 18:58:42 +0300

    0.0.3-alpha

---
+++ android/app/src/main/kotlin/org/jazzme/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.jazzme
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/UI/button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class JazzBTN extends StatelessWidget {
+  String title;
+  int fillColor;
+  int splashColor;
+  int textColor;
+
+  JazzBTN(
+      {this.title,
+      this.fillColor,
+      this.textColor,
+      this.splashColor,
+      @required this.onPressed})
+      : assert(title != null);
+
+  final GestureTapCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      fillColor: Color(fillColor),
+      splashColor: Color(splashColor),
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(24, 14, 24, 14),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(title,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Color(textColor),
+                    fontFamily: "TTCommons",
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w600))
+          ],
+        ),
+      ),
+      onPressed: onPressed,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(18.0),
+      ),
+    );
+  }
+}
